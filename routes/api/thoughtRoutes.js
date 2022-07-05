27dc5b624539dd7b0c4c27d8ed83dcf5dc33b406
bkhandag <khandagale.b.g@gmail.com>
2022-07-04 20:54:34 -0700

    added thoughtRoutes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  removeUser,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router
+  .route('/:userId')
+  .get(getSingleUser)
+  .put(updateUser)
+  .delete(removeUser);
+
+module.exports = router;
